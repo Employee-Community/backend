548370dd5a06fed7b8670af09f1dbd599b4222ef
SeungJun-Kwon <80217301+SeungJun-Kwon@users.noreply.github.com>
2025-09-08 12:18:01 +0900

    Refactor/redis ksj (#8)
    * feat : Redis 공통 서비스 구현
    
    * fix : public 추가
    
    * refactor : 기존 RedisTemplate 주입 받는 부분을 RedisService로 변경

---
+++ src/main/java/com/community/backend/common/service/RedisService.java
@@ -0,0 +1,23 @@
+package com.community.backend.common.service;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public interface RedisService<T> {
+
+    public void setValue(String key, T value);
+
+    public void setValue(String key, T value, Long duration);
+
+    public void setValue(String key, T value, Long duration, TimeUnit timeUnit);
+
+    public void incrementValue(String key, Long delta);
+
+    public T getValue(String key);
+
+    public Boolean hasKey(String key);
+
+    public Set<String> getKeys(String keyPattern);
+
+    public void deleteValue(String key);
+}
